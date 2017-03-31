8c9f452b7d371813de51bd1a149277fe04b030e9
Robert Tisma <robert.tisma@oicr.on.ca>
2017-03-31 08:13:14 -0400

    added DaoFactory and FileRestorer

---
+++ pcawg-import-client/src/main/java/org/icgc/dcc/pcawg/client/data/factory/AbstractDaoFactory.java
@@ -0,0 +1,72 @@
+package org.icgc.dcc.pcawg.client.data.factory;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.icgc.dcc.pcawg.client.data.AbstractFileDao;
+import org.icgc.dcc.pcawg.client.data.sample.SearchRequest;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.icgc.dcc.pcawg.client.download.Storage.downloadFileByURL;
+
+@RequiredArgsConstructor
+@Getter
+@Slf4j
+public abstract class AbstractDaoFactory<B, R extends SearchRequest<R>, D extends AbstractFileDao<B, R>> {
+  // take input url that should be used to download local_file
+  // take local_file, record if exists
+  // take persitance dat file, record if exists
+
+  /*
+       inputFnExists, outputFnExists
+  if ( 0, 0) ->  download inputFn using url, and store to .dat
+  if ( 0, 1) -> download inputFn using url, delete .dat, and store .dat
+  if ( 1, 0) -> do not download, load the inputFn and parse it like normal, store .dat
+  if ( 1, 1) -> do not download and do not load inputFn. Just restore .dat
+   */
+
+  @NonNull private final String downloadUrl;
+  @NonNull private final String inputFilename;
+  @NonNull private final FileRestorer<D> fileRestorer;
+
+  @SuppressWarnings("unchecked")
+  public final D getObject() throws IOException, ClassNotFoundException {
+    List<String> a;
+    val inputPath = Paths.get(inputFilename);
+    val inputFileExists = Files.exists(inputPath);
+    val persistedFileExists = fileRestorer.isPersisted();
+
+    D dao = null;
+    if (inputFileExists && persistedFileExists){
+      log.info("Inputfile [{}] and persistanceFile [{}] exist, deserializing...", inputFilename, fileRestorer.getPersistedFilename());
+      dao = fileRestorer.restore();
+    } else if (inputFileExists && !persistedFileExists){
+      log.info("Inputfile [{}] exists and persistanceFile [{}] DNE, creating new DAO and persisting it...", inputFilename, fileRestorer.getPersistedFilename());
+      dao = newObject(getInputFilename());
+      fileRestorer.store(dao);
+    } else if (!inputFileExists && persistedFileExists) {
+      log.info("Inputfile [{}] DNE and persistanceFile [{}] exists, cleaning everyting, creating new DAO and persisting it...", inputFilename, fileRestorer.getPersistedFilename());
+      val inputFile = downloadFileByURL(getDownloadUrl(), getInputFilename());
+      fileRestorer.clean();
+      dao = newObject(inputFile.getAbsolutePath());
+      fileRestorer.store(dao);
+    } else {
+      log.info("Inputfile [{}] DNE and persistanceFile [{}] DNE, download, creating new DAO and persisting it...", inputFilename, fileRestorer.getPersistedFilename());
+      val inputFile = downloadFileByURL(getDownloadUrl(), getInputFilename());
+      dao = newObject(inputFile.getAbsolutePath());
+      fileRestorer.store(dao);
+    }
+    return dao;
+  }
+
+  protected abstract D newObject(String filename);
+
+
+}
+
--- pcawg-import-client/src/test/java/org/icgc/dcc/pcawg/client/download/SampleMetadataDAOTest.java
+++ pcawg-import-client/src/test/java/org/icgc/dcc/pcawg/client/download/SampleMetadataDAOTest.java
@@ -1,15 +1,25 @@
 package org.icgc.dcc.pcawg.client.download;
 
+import com.google.common.base.Stopwatch;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
+import org.assertj.core.util.Sets;
 import org.icgc.dcc.pcawg.client.core.Factory;
 import org.icgc.dcc.pcawg.client.data.IcgcFileIdDao;
 import org.icgc.dcc.pcawg.client.data.SampleMetadataDAO;
+import org.icgc.dcc.pcawg.client.data.factory.impl.BarcodeBeanDaoFactory;
+import org.icgc.dcc.pcawg.client.data.factory.impl.SampleBeanDaoFactory;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.icgc.dcc.pcawg.client.config.ClientProperties.BARCODE_SHEET_TSV_URL;
+import static org.icgc.dcc.pcawg.client.config.ClientProperties.SAMPLE_SHEET_TSV_URL;
 import static org.icgc.dcc.pcawg.client.core.Factory.newFastFileSampleMetadataBeanDAOAndDownload;
 import static org.icgc.dcc.pcawg.client.core.Factory.newFileSampleMetadataBeanDAOAndDownload;
 import static org.icgc.dcc.pcawg.client.core.Factory.newFileSampleMetadataDAOOldAndDownload;
@@ -72,4 +82,55 @@ public class SampleMetadataDAOTest {
 
   }
 
+
+  @Test
+  @SneakyThrows
+  public void testSampleBeanDaoFactory(){
+    val inputFilename = "test.sample.tsv";
+    val persistFilename= "test.sample.dat";
+    Files.deleteIfExists(Paths.get(inputFilename));
+    Files.deleteIfExists(Paths.get(persistFilename));
+    Storage.downloadFileByURL(SAMPLE_SHEET_TSV_URL,inputFilename);
+    val factory = new SampleBeanDaoFactory(SAMPLE_SHEET_TSV_URL,inputFilename, persistFilename);
+    val watch = Stopwatch.createStarted();
+    val barcodeBeanDao = factory.getObject();
+    watch.stop();
+    log.info("First Time: {}", watch.elapsed(TimeUnit.MILLISECONDS));
+    watch.reset();
+
+    watch.start();
+    val factory2 = new SampleBeanDaoFactory(BARCODE_SHEET_TSV_URL,inputFilename, persistFilename);
+    val barcodeBeanDao2 = factory2.getObject();
+    watch.stop();
+    log.info("Second Time: {}", watch.elapsed(TimeUnit.MILLISECONDS));
+    assertThat(barcodeBeanDao.findAll()).containsAll(barcodeBeanDao2.findAll());
+
+  }
+
+  @Test
+  @SneakyThrows
+  public void testBarcodeBeanDaoFactory(){
+    val inputFilename = "test.barcode.tsv";
+    val persistFilename= "test.barcode.dat";
+    Files.deleteIfExists(Paths.get(inputFilename));
+    Files.deleteIfExists(Paths.get(persistFilename));
+    Storage.downloadFileByURL(BARCODE_SHEET_TSV_URL,inputFilename);
+    val factory = new BarcodeBeanDaoFactory(BARCODE_SHEET_TSV_URL,inputFilename, persistFilename);
+    val watch = Stopwatch.createStarted();
+    val barcodeBeanDao = factory.getObject();
+    watch.stop();
+    log.info("First Time: {}", watch.elapsed(TimeUnit.MILLISECONDS));
+    watch.reset();
+
+    watch.start();
+    val factory2 = new BarcodeBeanDaoFactory(BARCODE_SHEET_TSV_URL,inputFilename, persistFilename);
+    val barcodeBeanDao2 = factory2.getObject();
+    watch.stop();
+    log.info("Second Time: {}", watch.elapsed(TimeUnit.MILLISECONDS));
+    val set1 = Sets.newHashSet(barcodeBeanDao.findAll());
+    val set2 = Sets.newHashSet(barcodeBeanDao2.findAll());
+    assertThat(set1.containsAll(set2));
+    assertThat(set2.containsAll(set1));
+
+  }
 }
